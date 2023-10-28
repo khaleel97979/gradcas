099be7feb1065ea91444d4255dd217400fa1604f
khaleel97979 <82385929+khaleel97979@users.noreply.github.com>
2023-10-28 09:37:13 +0100

    v00p

--- scripts.js
+++ scripts.js
@@ -1,21 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // Accordion Functionality
-  const acc = document.getElementsByClassName("accordion");
-  for (let i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      const panel = this.nextElementSibling;
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-      }
-    });
-  }
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Accordion Functionality
+    var acc = document.getElementsByClassName("accordion");
+    for (var i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
 
-  // Tab Functionality
-  document.getElementById("defaultOpen").click();
-  const tabLinks = document.getElementsByClassName("tab-links");
-  for (let i = 0; i < tabLinks.length; i++) {
-    tabLinks[i].addEventListener("click", function(event) {
-      openTab(event, this.getAttribute('onclick').split("'
+    // Tab Functionality
+    window.openTab = function (tabName) {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tab-content");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tab-links");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(tabName).style.display = "block";
+        event.currentTarget.className += " active";
+    }
+
+    // Open default tab
+    document.getElementById("defaultOpen").click();
+});

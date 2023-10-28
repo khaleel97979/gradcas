a56399429890b5b74d34e16fc3045edd83b61a23
khaleel97979 <82385929+khaleel97979@users.noreply.github.com>
2023-10-28 09:33:55 +0100

    v00o

--- scripts.js
+++ scripts.js
@@ -1,12 +1,10 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
   // Accordion Functionality
-  var acc = document.getElementsByClassName("accordion");
-  var i;
-
-  for (i = 0; i < acc.length; i++) {
+  const acc = document.getElementsByClassName("accordion");
+  for (let i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
       this.classList.toggle("active");
-      var panel = this.nextElementSibling;
+      const panel = this.nextElementSibling;
       if (panel.style.maxHeight) {
         panel.style.maxHeight = null;
       } else {
@@ -17,24 +15,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
   // Tab Functionality
   document.getElementById("defaultOpen").click();
-
-  document.querySelectorAll('.tab-links').forEach(tab => {
-    tab.addEventListener('click', function(event) {
-      openTab(event, this.getAttribute('onclick').split("'")[1]);
-    });
-  });
-
-  window.openTab = function (evt, tabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tab-links");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
-});
+  const tabLinks = document.getElementsByClassName("tab-links");
+  for (let i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].addEventListener("click", function(event) {
+      openTab(event, this.getAttribute('onclick').split("'

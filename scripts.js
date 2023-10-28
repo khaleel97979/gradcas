0b0da4f751de47bce087e83678d1ceacec2eefe7
khaleel97979 <82385929+khaleel97979@users.noreply.github.com>
2023-10-28 09:22:13 +0100

    v00m

--- scripts.js
+++ scripts.js
@@ -14,12 +14,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
   }
 
   // Tab Functionality
-  window.openTab = function (tabName) {
-    var i, tabcontent;
+  window.openTab = function (evt, tabName) {
+    var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
+    tablinks = document.getElementsByClassName("tab-links");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
     document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
   }
+
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
 });

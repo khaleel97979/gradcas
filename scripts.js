374a454d1626086d28d814c7b95c1451a21b0dfa
khaleel97979 <82385929+khaleel97979@users.noreply.github.com>
2023-10-28 09:13:10 +0100

    v00l

--- scripts.js
+++ scripts.js
@@ -13,7 +13,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
   }
 
-  // Add any other JavaScript functionalities below
-  // ...
-
+  // Tab Functionality
+  window.openTab = function (tabName) {
+    var i, tabcontent;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+  }
 });
